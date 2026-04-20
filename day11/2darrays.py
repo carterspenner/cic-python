c15e1043925ab775d553b0a493542dbd75d18f09
Carter Spenner <carter.spenner@gmail.com>
2026-04-20 12:13:55 -0600

    fix day11

--- day11/2darrays.py
+++ day11/2darrays.py
@@ -22,6 +22,15 @@ for l in range(20):
 print(f"Total Sum:\n{totalSum}")
 
 #100% code
-array2 = numpy.where(array2 % 2 == 0, 0, array2)
-array2 = numpy.where(array2 % 3 == 0, 0, array2)
-print(f"Filtered Array:\n{array2}")
+# array2 = numpy.filter(array2 % 2 == 0, 0, array2)
+# array2 = numpy.where(array2 % 3 == 0, 0, array2)
+# print(f"Filtered Array:\n{array2}")
+
+filter_arr = []
+for l in range(20):
+    for w in range(20):
+        # if the element is higher than 42, set the value to True, otherwise False:
+        if not array2[l][w] % 2 == 0 and not array2[l][w] % 3 == 0:
+            filter_arr.append(int(array2[l][w]))
+
+print(f"Filtered Array:\n{filter_arr}")
